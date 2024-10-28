cf68ef78a931d081729a531fc48099be3cc71767
aeonics <aeonics@live.no>
2024-10-27 22:25:04 -0600

    Ejemplo IfElse

--- IF-ELSE/ ph_levels.py
+++ IF-ELSE/ ph_levels.py
@@ -1,10 +1,19 @@
-if grade > 90:
-  print('A')
-elif grade > 80:
-  print('B')
-elif grade > 70:
-  print('C')
-elif grade > 60:
-  print('D')
+"""
+En quimica, el pH es una escala que se usa para espesificar la acidez
+Crea un programa que cheque cuando el niveld e pH sea basico, acido o neutral
+Tip:
+Crea una variable pH y pregunta al usuario un valor entre 0 y 14
+Crea sentencias if, elif, else:
+    Si ph es mayor que 7, escribe "Basic".
+    Si ph es menor que 7, escribe  "Acidic".
+    Else, escribe "Neutral".
+"""
+
+pH = int(input("Coloca un valor de 0 a 14: "))
+
+if pH > 7:
+    print("Basic")
+elif pH < 7:
+    print("Acidic")
 else:
-  print('F')
+    print("Neutral")
