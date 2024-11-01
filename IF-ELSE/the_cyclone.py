4b0cb01e8835c56f6ada771e9eb53d45e18430dc
aeonics <aeonics@live.no>
2024-10-31 22:59:01 -0600

    The Cyclone

---
+++ IF-ELSE/the_cyclone.py
@@ -0,0 +1,20 @@
+"""Desde 1927, la montaña rusa "The Cyclone" ha deleitando a los visitantes de Coney Island (Brooklyn, NY). -
+Ahora están instalando un nuevo sistema de entrada (el requisito de altura es de 137 cm y el costo es de 10 créditos) y necesita su ayuda para escribir el programa para ello.
+
+
+Pregúntale al usuario cuál es su altura y cuántos créditos tiene, y almacena los valores en una variable de altura y en una variable de créditos.
+
+Utilice una combinación de operadores relacionales y lógicos para crear las reglas:
+
+    Si son lo suficientemente altos y tienen suficientes créditos, imprime "Disfrute el paseo".
+    Si tienen suficientes créditos, pero no son lo suficientemente altos, print "No eres lo suficientemente alto para montar".
+    Si son lo suficientemente altos, pero no tienen suficientes créditos, imprime "No tienes suficientes créditos".
+    El mismo, imprime un mensaje diciendo que no han cumplido ninguno de los requisitos."""
+
+print("Nuevo sistema de entrada para  \"The Cyclone\" ")
+
+altura = float(input("¿Cual es tu altura en cm?: "))
+creditos = int(input("¿Cuantos creditos tienes: "))
+
+
+
